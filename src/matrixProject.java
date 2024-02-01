37059e22703e47cf8c02a174fb6f0f66e04af5c2
Saltanat Kydykbekova <saltanat.kydykbekova1@gmail.com>
2024-02-01 13:25:35 +0600

    triple ships are protected

--- src/matrixProject.java
+++ src/matrixProject.java
@@ -6,13 +6,14 @@ public class matrixProject {
             Random random = new Random();
             int gameFieldLength = 7;
             char water = '~';
-            char ship = 's';
+            char ship3 = '3';
             char hit = 'h';
             char miss = 'm';
+            char protection = 'p';
             int singleShipNumber = 4;
             int doubleShipNumber = 2;
             int tripleShipNumber = 1;
-            char[][] gameBoard = createGameBoard(gameFieldLength,water,ship,singleShipNumber,doubleShipNumber,tripleShipNumber,random);
+            char[][] gameBoard = createGameBoard(gameFieldLength,water,ship3,singleShipNumber,doubleShipNumber,tripleShipNumber,random,protection);
             for (int i = 0;i<gameFieldLength;i++)
             {
                 for (int j = 0;j<gameFieldLength;j++)
@@ -23,7 +24,7 @@ public class matrixProject {
             }
         }
     
-        private static char[][] createGameBoard(int gameFieldLength, char water, char ship, int singleShipNumber, int doubleShipNumber, int tripleShipNumber,Random random) 
+        private static char[][] createGameBoard(int gameFieldLength, char water, char ship3, int singleShipNumber, int doubleShipNumber, int tripleShipNumber,Random random,char protection) 
         {
             char[][] gameBoard = new char[gameFieldLength][gameFieldLength];
             int minCase = 1;
@@ -32,16 +33,16 @@ public class matrixProject {
             int placedTripleShips = 0;
             int placedDoubleShips = 0;
             int placedSingleShips = 0;
-            // int minBorder = 1;
-            // int maxBorder = gameFieldLength-1;
+            int minBorder = 1;
+            int maxBorder = gameFieldLength-1;
             for(char[] row : gameBoard)
             {
                 Arrays.fill(row,water);
             }
             while(placedTripleShips<tripleShipNumber)
             {
-                int minBorder = 1;
-                int maxBorder = gameFieldLength-1;
+                // int minBorder = 1;
+                // int maxBorder = gameFieldLength-1;
                 for(int i = 0;i<coordinates.length;i++)
                 {
             // int minBorder = 1;
@@ -50,21 +51,50 @@ public class matrixProject {
                 }
                 if(gameBoard[coordinates[0]][coordinates[1]]==water)
                 {
-                    gameBoard[coordinates[0]][coordinates[1]]=ship;
+                    gameBoard[coordinates[0]][coordinates[1]]=ship3;
                     int randomCase = minCase + random.nextInt(maxCase - minCase);
                     switch (randomCase)
                     {
                         case 1://vertical
-                            gameBoard[coordinates[0]+1][coordinates[1]]=ship;
-                            gameBoard[coordinates[0]-1][coordinates[1]]=ship;
-                            break;
-                    
+                            gameBoard[coordinates[0]+1][coordinates[1]]=ship3;
+                            gameBoard[coordinates[0]-1][coordinates[1]]=ship3;
+                            if(coordinates[0]-1==0)//set borders to triple ship
+                            {
+                                for (int i = 0;i<4;i++)
+                                {
+                                    gameBoard[coordinates[0]-1+i][coordinates[1]+1]=protection;
+                                    gameBoard[coordinates[0]-1+i][coordinates[1]-1]=protection;
+                                }
+                                gameBoard[coordinates[0]+2][coordinates[1]]=protection;
+                            }
+                            else if(coordinates[0]+1==6)
+                            {
+                                gameBoard[coordinates[0]-2][coordinates[1]]=protection;
+                                for (int i = 0;i<4;i++)
+                                {
+                                    gameBoard[coordinates[0]-2+i][coordinates[1]+1]=protection;
+                                    gameBoard[coordinates[0]-2+i][coordinates[1]-1]=protection;
+                                }
+                                
+                            }
+                            else
+                            {
+                                gameBoard[coordinates[0]-2][coordinates[1]]=protection;
+                                for (int i = 0;i<5;i++)
+                                {
+                                    gameBoard[coordinates[0]-2+i][coordinates[1]+1]=protection;
+                                    gameBoard[coordinates[0]-2+i][coordinates[1]-1]=protection;
+                                }
+                                gameBoard[coordinates[0]+2][coordinates[1]]=protection;
+                            }
+                            break;                  
                         case 2://horizontal
-                            gameBoard[coordinates[0]][coordinates[1]+1]=ship;
-                            gameBoard[coordinates[0]][coordinates[1]-1]=ship;
+                            gameBoard[coordinates[0]][coordinates[1]+1]=ship3;
+                            gameBoard[coordinates[0]][coordinates[1]-1]=ship3;
                             break;
                     }
                     placedTripleShips++;
+                    
                 }
             }
             // while (placedDoubleShips<doubleShipNumber)
